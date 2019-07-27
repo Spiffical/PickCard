dd9d87cdbfa430afa99f4faf52a17ff8e0ae3278
Spencer Bialek <spencer.bialek@gmail.com>
2019-07-26 18:20:04 -0700

    reliably collects picard dialog

--- pick_card.py
+++ pick_card.py
@@ -1,15 +1,36 @@
 import numpy as np
 import glob
+import os
 
+# Collect files
 path = r'/Users/spencerbialek/Documents/tng-transcripts/Scripts - TNG/*.txt'
-all_files = glob.iglob(path)
+all_files = glob.glob(path)
+
+# Sort files
+indices = np.argsort([int(os.path.basename(f)[:-4]) for f in all_files])
+all_files = np.asarray(all_files)[indices]
 
 picard_script = []
-start_recording = False
+dialog_chunk = ''
+recording = False
 for f in all_files:
-    script = np.genfromtxt(f, dtype=str, delimiter='\n')
+    script = np.genfromtxt(f, dtype=str, delimiter='\n', encoding='latin1')
     for line in script:
-        if 'picard' in line.lower():
+        if recording:
+            # Dialog lines always begin with three '\t' characters
+            if line.count('\t') == 3:
+                line = line.strip('\t')
+                dialog_chunk += ' {}'.format(line)
+            else:
+                if dialog_chunk == '':
+                    continue
+                else:
+                    picard_script.append(dialog_chunk[1:])
+                    dialog_chunk = ''
+                    recording = False
 
-            picard_script.append(line)
+        # Picard dialog always begins with 'PICARD'
+        if 'PICARD' in line:
+            recording = True
+
 
