708812dd8f256eeef4125412720cad9380e083e2
sabjorn <info@sabjorn.net>
2019-07-27 20:27:10 -0700

    Add datamodel for handling all the different types of paragraph.
    Script now breaks up dialog for Picard and Data. Breaking into questions now happens in a second step.

---
+++ datamodel.py
@@ -0,0 +1,10 @@
+characters = ["PICARD", "DATA"]
+
+# this could be a class in the future so that other functions can rely on it
+datamodel = {
+    "type": "",
+    "character": None,
+    "body": ""
+}
+
+datatypes = ["heading", "action", "dialogue", "transition"]
--- pick_card.py
+++ pick_card.py
@@ -2,6 +2,39 @@ import numpy as np
 import glob
 import os
 
+import datamodel
+
+def breakApartDialog(script, character):
+    scipt_exract = []
+    dialog_chunk = ''
+    recording = False
+
+    for line in script:
+        if character in line:
+            recording = True
+            data = datamodel.datamodel.copy()
+            data["type"] = "dialog"
+            data["character"] = character
+            continue  # Skip line only containing character name
+        if recording:
+            # Dialog lines always begin with three '\t' characters and occur immediately after character name line
+            if line.count('\t') == 3:
+                line = line.strip('\t')
+                dialog_chunk += ' {}'.format(line)
+            else:
+                data["body"] = dialog_chunk
+                scipt_exract.append(data)
+                dialog_chunk = ''
+                recording = False
+    return scipt_exract
+
+def splitDialogByType(dialog, delimiter="?"):
+    output = []
+    for elem in dialog:
+        if delimiter in elem["body"]:
+            output.append(elem)
+    return output
+
 # Collect files
 path = r'./data/Scripts - TNG/*.txt'
 all_files = glob.glob(path)
@@ -9,28 +42,14 @@ all_files = glob.glob(path)
 # Sort files
 indices = np.argsort([int(os.path.basename(f)[:-4]) for f in all_files])
 all_files = np.asarray(all_files)[indices]
-
-picard_script = []
-dialog_chunk = ''
-recording = False
+all_dialog = []
 for f in all_files:
     script = np.genfromtxt(f, dtype=str, delimiter='\n', encoding='latin1')
-    for line in script:
-        # Picard dialog always begins with 'PICARD'
-        if 'PICARD' in line:
-            recording = True
-            continue  # Skip line only containing Picard's name
-        if recording:
-            # Dialog lines always begin with three '\t' characters and occur immediately after character name line
-            if line.count('\t') == 3:
-                line = line.strip('\t')
-                dialog_chunk += ' {}'.format(line)
-            else:
-                if '?' in dialog_chunk:
-                    picard_script.append(dialog_chunk[1:])
-                dialog_chunk = ''
-                recording = False
+    for character in datamodel.characters:
+        all_dialog += breakApartDialog(script, character)
 
+picard = [x for x in all_dialog if x['character'] is 'PICARD']
+data = [x for x in all_dialog if x['character'] is 'DATA']
 
-
+picard_questions = splitDialogByType(picard, delimiter="?")
 
